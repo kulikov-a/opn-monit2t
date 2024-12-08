45f8d1978870915232729e7af12e2a63fc18c4e1
kulikov-a <36099472+kulikov-a@users.noreply.github.com>
2024-12-08 15:59:08 +0300

    release 1.2
    message_thread_id support

--- src/opnsense/scripts/OPNsense/Monit2T/monit2t.py
+++ src/opnsense/scripts/OPNsense/Monit2T/monit2t.py
@@ -11,12 +11,15 @@ import requests
 from configparser import ConfigParser
 
 
-def t_send(token, chat_id, message):
+def t_send(token, chat_id, message_thread_id, message):
     t_url = f'https://api.telegram.org/bot{token}/sendMessage'
     syslog.openlog('monit')
     try:
         # restore multiline (if any) message and send to endpoint
-        resp = requests.post(t_url, json={'chat_id': chat_id, 'text': message.replace(r'\n', '\n'), 'parse_mode': 'HTML'})
+        if message_thread_id:
+            resp = requests.post(t_url, json={'chat_id': chat_id, 'message_thread_id': message_thread_id, 'text': message.replace(r'\n', '\n'), 'parse_mode': 'HTML'})
+        else:
+            resp = requests.post(t_url, json={'chat_id': chat_id, 'text': message.replace(r'\n', '\n'), 'parse_mode': 'HTML'})
     except requests.exceptions.Timeout:
         syslog.syslog(syslog.LOG_ERR, 'Telegram API endpoint request timeout')
         sys.exit('Telegram API endpoint request timeout')
@@ -39,13 +42,14 @@ if os.path.exists(t_conf):
     cnf.read(t_conf)
     token = str(cnf['api_settings']['token'])
     chat_id = str(cnf['api_settings']['chat_id'])
+    message_thread_id = str(cnf['api_settings']['message_thread_id'])
     message = str(cnf['alert_settings']['message'])
     if len(sys.argv) == 1:
         msg_vars = re.findall(r"{([^{]*?)}", message)
         env_vars={}
         for msg_var in msg_vars:
             env_vars[msg_var] = os.getenv(msg_var, "null").replace('<','&lt').replace('>','&gt')
-        t_send(token, chat_id, message.format(**env_vars))
+        t_send(token, chat_id, message_thread_id, message.format(**env_vars))
     else:
         message = 'This is a test telegram message\nAlerts will be sent in the following format: \n\n' + message
-        t_send(token, chat_id, message)
+        t_send(token, chat_id, message_thread_id, message)
